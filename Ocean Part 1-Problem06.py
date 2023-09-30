185b683a36f543eb353a5febfbf9805a5f01f674
umangkbhatt <umangkbhatt@live.com>
2023-09-30 13:14:12 +0530

    added an updated solution for problem 6th

---
+++ Ocean Part 1-Problem06.py
@@ -0,0 +1,6 @@
+# Write a program to take as input a number n  and display the first n natural numbers 
+
+n = int(input("Enter natural number of your choice: "))
+
+for i in range(1,n+1):
+    print(i)
